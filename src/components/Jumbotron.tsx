f8fa81f1ebc784c42765aec6944c03846eba329e
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2023-08-29 15:35:20 -0400

    Fixed Learn More Button Link Issue
    Fixed issue where the learn more button would not work when user clicks mouse on the corner. Replaced parent element from <div> to <a> and it works

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import React, { FC, useState } from "react";
 
 const Jumbotron: FC = () => {
-  const { height, width } = useWindowDimensions();
+  const { width } = useWindowDimensions();
 
   const [hover, setHover] = useState(false);
 
@@ -22,14 +22,13 @@ const Jumbotron: FC = () => {
       <h1 className="text-white m-5 animate__animated animate__fadeInDown">
         The World&apos;s Most Advanced HUD App
       </h1>
-      <div
+      <a
+        href="#features"
         className="mt-24 h-10 w-32 rounded-3xl flex items-center justify-center cursor-pointer hover:w-40 bg-gradient-to-r from-cyan-200 to-blue-300 dark:from-cyan-500 dark:to-blue-600 hover:shadow-lg duration-300"
         onMouseOver={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
       >
-        <a href="#features" className="duration-300">
-          Learn More
-        </a>
+        Learn More
         {hover && (
           <FontAwesomeIcon
             icon={faAnglesDown}
@@ -37,7 +36,7 @@ const Jumbotron: FC = () => {
             size="1x"
           />
         )}
-      </div>
+      </a>
       <img
         src={"/images/appstoredownload.png"}
         alt="App Store Download"
