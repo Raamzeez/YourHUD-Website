505e4ce659dc30e8cd855a2a8256aebbf1de0fc2
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2023-08-29 15:16:02 -0400

    Fixed Mobile Link Issue
    Fixed issue where users could not click on links on mobile. The issue had to do with parent div having margin. Also simplified code by storing links in an external file, and then using .map() to render them

---
+++ src/data/links.ts
@@ -0,0 +1,24 @@
+const links: iLink[] = [
+  {
+    url: "/#features",
+    title: "Features",
+  },
+  {
+    url: "/#gallery",
+    title: "Gallery",
+  },
+  {
+    url: "/#demo",
+    title: "Demo",
+  },
+  {
+    url: "/contact",
+    title: "Contact",
+  },
+  {
+    url: "/privacy",
+    title: "Privacy",
+  },
+];
+
+export default links;
